93cc7a9f05b7e58024f84485ee3a887397155268
Thomas Baxter <thomas@thomasbaxter.info>
2025-04-08 21:00:25 +1000

    Update Dep: custom_components/aus_space_weather via HACS

---
+++ custom_components/aus_space_weather/const.py
@@ -0,0 +1,15 @@
+DOMAIN = "aus_space_weather"
+
+# List of supported locations for the K index
+LOCATIONS = [
+    "Australian region",
+    "Hobart",
+    "Canberra",
+    "Sydney",
+    "Melbourne",
+    "Brisbane",
+    "Perth",
+    "Adelaide",
+    "Darwin",
+    "Alice Springs"
+]
